63c1f6ef8357f8f63220075d6fd7d762b4db1180
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-11-13 16:12:15 -0500

    Added glances 3.3.0.4

---
+++ 3.3.0.4/modulefile.lua
@@ -0,0 +1,35 @@
+--
+-- glances 3.3.0.4 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: Glances is a cross-platform system monitoring tool written in Python."
+-- "Keywords: singularity utilities"
+
+whatis("Name: glances")
+whatis("Version: 3.3.0.4")
+whatis("Category: Other")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: Glances is a cross-platform system monitoring tool written in Python.")
+whatis("Keywords: singularity utilities")
+
+help([[
+Glances is a cross-platform system monitoring tool written in Python.
+
+To load the module, type
+
+> module load glances/3.3.0.4
+
+To unload the module, type
+
+> module unload glances/3.3.0.4
+
+Tools included in this module are
+
+* glances
+]])
+
+local package = "glances"
+local version = "3.3.0.4"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
